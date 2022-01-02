c9dcf915a4a24c6afbbc3e2ee3b097fe48b3a8da
Gospodi <gospodijazzet@gmail.com>
2022-01-02 18:01:14 +0300

    Добавляем версию приложения для MacOS

---
+++ MacLandmarks/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  MacLandmarks
+//
+//  Created by Gospodi on 02.01.2022.
+//  Copyright © 2022 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
