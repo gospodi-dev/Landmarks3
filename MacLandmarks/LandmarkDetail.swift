ba4e1c721244fc2606ff5166aecfe13e63f9a40e
Gospodi <gospodijazzet@gmail.com>
2022-01-02 18:55:27 +0300

    Добавим кнопку «Открыть на картах»

--- MacLandmarks/LandmarkDetail.swift
+++ MacLandmarks/LandmarkDetail.swift
@@ -1,11 +1,5 @@
-/*
-See LICENSE folder for this sample’s licensing information.
-
-Abstract:
-A view showing the details for a landmark.
-*/
-
 import SwiftUI
+import MapKit
 
 struct LandmarkDetail: View {
     @EnvironmentObject var modelData: ModelData
@@ -17,38 +11,46 @@ struct LandmarkDetail: View {
 
     var body: some View {
         ScrollView {
-            MapView(coordinate: landmark.locationCoordinate)
-                .ignoresSafeArea(edges: .top)
-                .frame(height: 300)
-            
+            ZStack(alignment: Alignment(horizontal: .trailing, vertical: .top)) {
+                MapView(coordinate: landmark.locationCoordinate)
+                    .ignoresSafeArea(edges: .top)
+                    .frame(height: 300)
+
+                Button("Open in Maps") {
+                    let destination = MKMapItem(placemark: MKPlacemark(coordinate: landmark.locationCoordinate))
+                    destination.name = landmark.name
+                    destination.openInMaps()
+                }
+                .padding()
+            }
+
             VStack(alignment: .leading, spacing: 20) {
-                HStack(spacing:24) {
+                HStack(spacing: 24) {
                     CircleImage(image: landmark.image.resizable())
                         .frame(width: 160, height: 160)
-                }
-
-                VStack(alignment: .leading) {
-                    HStack {
-                        Text(landmark.name)
-                            .font(.title)
-                        FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
-                            .buttonStyle(.plain)
-                    }
 
                     VStack(alignment: .leading) {
-                        Text(landmark.park)
-                        Text(landmark.state)
+                        HStack {
+                            Text(landmark.name)
+                                .font(.title)
+                            FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
+                                .buttonStyle(.plain)
+                        }
+
+                        VStack(alignment: .leading) {
+                            Text(landmark.park)
+                            Text(landmark.state)
+                        }
+                        .font(.subheadline)
+                        .foregroundColor(.secondary)
                     }
-                    .font(.subheadline)
-                    .foregroundColor(.secondary)
-
-                    Divider()
-
-                    Text("About \(landmark.name)")
-                        .font(.title2)
-                    Text(landmark.description)
                 }
-                
+
+                Divider()
+
+                Text("About \(landmark.name)")
+                    .font(.title2)
+                Text(landmark.description)
             }
             .padding()
             .frame(maxWidth: 700)
