eeddd61a5fbb3782b6352c1e6da09c23e47392c1
Gospodi <gospodijazzet@gmail.com>
2022-01-04 17:52:41 +0300

    Добавим набор команд отвечающих за переключения боковой панели

---
+++ MacLandmarks/LandmarkCommands.swift
@@ -0,0 +1,15 @@
+//
+//  LandmarkCommands.swift
+//  CreatingAwatchOSApp
+//
+//  Created by Gospodi on 04.01.2022.
+//  Copyright © 2022 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkCommands: Commands {
+    var body: some Commands {
+        SidebarCommands()
+    }
+}
