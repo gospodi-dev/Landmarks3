977efb1d8700978f6347d295a8efa698cd27988d
Gospodi <gospodijazzet@gmail.com>
2022-01-04 18:51:40 +0300

    Чтобы SwiftUI обновлял карту при каждом изменении delta, изменим способ расчета и применить region.

---
+++ MacLandmarks/LandmarkSettings.swift
@@ -0,0 +1,21 @@
+//
+//  LandmarkSettings.swift
+//  MacLandmarks
+//
+//  Created by Gospodi on 04.01.2022.
+//  Copyright © 2022 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkSettings: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandmarkSettings_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkSettings()
+    }
+}
